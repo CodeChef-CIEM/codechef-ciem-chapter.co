89aa7e3520e6f161dd013a9234b6cea931a20f46
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2021-09-12 08:56:02 +0530

    Attempt to Deploy Temporary Website Version 2, Backups for Version 1 in Folder

---
+++ assets/JS/index.js
@@ -0,0 +1,10 @@
+function Drop_Nav()
+{
+    document.getElementById('Navigate').style.display="none";
+    document.getElementById('nav_show').style.display="block";
+}
+function Show_Nav()
+{
+    document.getElementById('Navigate').style.display="block";
+    document.getElementById('nav_show').style.display="none";
+}
